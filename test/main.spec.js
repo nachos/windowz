b5ef0996d643fddb87242cddc0d46ca74f53575a
Noam Okman <noamokman@gmail.com>
2015-08-11 16:43:29 +0300

    fix tests

--- test/main.spec.js
+++ test/main.spec.js
@@ -3,22 +3,23 @@
 var chai = require('chai');
 var expect = chai.expect;
 
-var windowsMgr = require('../lib');
+describe('windowz', function () {
+  var windowz = require('../lib');
 
-describe('windows-manager', function () {
   describe('exports', function () {
-    expect(windowsMgr.getAll).to.be.a('function');
-    expect(windowsMgr.getTitle).to.be.a('function');
-    expect(windowsMgr.setToForeground).to.be.a('function');
-    expect(windowsMgr.minimize).to.be.a('function');
-    expect(windowsMgr.activate).to.be.a('function');
-    expect(windowsMgr.restore).to.be.a('function');
-    expect(windowsMgr.show).to.be.a('function');
-    expect(windowsMgr.maximize).to.be.a('function');
-    expect(windowsMgr.isForeground).to.be.a('function');
-    expect(windowsMgr.isMinimized).to.be.a('function');
-    expect(windowsMgr.setTopMost).to.be.a('function');
-    expect(windowsMgr.close).to.be.a('function');
-    expect(windowsMgr.getCurrent).to.be.a('function');
+    it('should expose api', function () {
+      expect(windowz.getAll).to.be.a('function');
+      expect(windowz.getTitle).to.be.a('function');
+      expect(windowz.setToForeground).to.be.a('function');
+      expect(windowz.minimize).to.be.a('function');
+      expect(windowz.activate).to.be.a('function');
+      expect(windowz.show).to.be.a('function');
+      expect(windowz.maximize).to.be.a('function');
+      expect(windowz.isForeground).to.be.a('function');
+      expect(windowz.isMinimized).to.be.a('function');
+      expect(windowz.setTopMost).to.be.a('function');
+      expect(windowz.close).to.be.a('function');
+      expect(windowz.getCurrent).to.be.a('function');
+    });
   });
 });
